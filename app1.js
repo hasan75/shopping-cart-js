bfbaab72c6b5dd5e443a438e25efd705d68d4275
Hasan75 <hasanjab14@gmail.com>
2021-08-20 22:44:13 +0600

    bug fixed for tag

--- app1.js
+++ app1.js
@@ -11,6 +11,28 @@ function updateProduct(product, price, isIncrease){
     const productTotal = document.getElementById(product + '-total')
     productTotal.innerText = productNumber * price
 
+    //calculating total price
+    calculateTotal()
+
+}
+function getInput(product){
+    const productInput = document.getElementById(product+'-number')
+    const productNumber = parseInt(productInput.value)
+    return productNumber
+}
+
+function calculateTotal(){
+    const phoneTotal = getInput('phone') * 1219;
+    const caseTOtal = getInput('case') * 59
+
+    const subTotal = phoneTotal + caseTOtal
+    document.getElementById('sub-total').innerText = subTotal
+
+    const tax = subTotal / 10
+    document.getElementById('tax-amount').innerText = tax
+
+    const total = subTotal + tax
+    document.getElementById('total-price').innerText = total
 }
 //phone
 document.getElementById('phone-plus').addEventListener('click',function(){
